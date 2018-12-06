57090fa972c9c7558b83ea5d78299c45711ea2a1
tenzing yeshi <tyeshi777@yahoo.com>
2018-12-06 19:55:09 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// add solution here
+function theBeatlesPlay(musicians, instruments) {
+  let album = [];
+  for (let i = 0; i < musicians.length; i++) {
+    album.push(`${musicians[i]} plays ${instruments[i]}`);
+  }
+  return album;
+}
+function johnLennonFacts(facts) {
+  newFacts = [];
+  let i = 0;
+  while (i < facts.length) {
+  newFacts.push(`${facts[i]}!!!`);
+  i++;
+  }
+  return newFacts;
+}
